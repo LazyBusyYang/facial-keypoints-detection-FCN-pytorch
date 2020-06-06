fcc886bb7b258718d1568bc6a4611fa05e6e9f66
LazyBusyYang <address_null@qq.com>
2020-06-06 14:26:21 +0800

    Add files via upload

--- Config.py
+++ Config.py
@@ -3,10 +3,10 @@ batch_size = 96
 data_load_number_worker = 8
 lr = 3e-3
 
-trainset_path = "/home1/gaoyang/facial-keypoints-detection/training.csv"
-testset_path = "/home1/gaoyang/facial-keypoints-detection/test.csv"
-idLookupTable = "/home1/gaoyang/facial-keypoints-detection/IdLookupTable.csv"
-ResultPath = "/home1/gaoyang/facial-keypoints-detection/result.csv"
-checkpoint_path = '/home1/gaoyang/models/Facial_FCN/checkpoint.pth.tar'
-modelbest_path = '/home1/gaoyang/models/Facial_FCN/model_best.tar'
+trainset_path = "/yourpath/facial-keypoints-detection/training.csv"
+testset_path = "/yourpath/facial-keypoints-detection/test.csv"
+idLookupTable = "/yourpath/facial-keypoints-detection/IdLookupTable.csv"
+ResultPath = "/yourpath/facial-keypoints-detection/result.csv"
+checkpoint_path = '/yourpath/models/Facial_FCN/checkpoint.pth.tar'
+modelbest_path = '/yourpath/models/Facial_FCN/model_best.tar'
 use_cuda = True
